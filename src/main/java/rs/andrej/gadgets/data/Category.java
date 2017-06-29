bd22aeb84c537ac095ba9d2d76fce11992be44ac
Radonca <Radonca@DESKTOP-K5MFEK9>
2017-06-29 09:22:20 +0200

    Updated Catagory

--- src/main/java/rs/andrej/gadgets/data/Category.java
+++ src/main/java/rs/andrej/gadgets/data/Category.java
@@ -33,7 +33,7 @@ public class Category {
 
     @Override
     public String toString() {
-        return "Category{" + "idCategory=" + idCategory + ", name=" + name + '}';
+        return "The category of the product is :{ The name is :=" + name + '}';
     }
       
     
